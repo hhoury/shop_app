1044f7492d00692d9d6387353c5d5488dc887c28
hhoury <hhoury@hotmail.com>
2022-04-08 11:46:50 +0300

    dissmissable

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -16,18 +16,32 @@ class CartItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundColor: Theme.of(context).primaryColor,
-            child: Padding(padding:const EdgeInsets.all(5),child: FittedBox(child: Text('\$$price'))),
+    return Dismissible(
+      
+      key: ValueKey(id),
+      background: Container(
+          color: Theme.of(context).errorColor,
+          child: Icon(Icons.delete, color: Colors.white, size: 40),
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right: 20),
+          margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),// to make the effect only behind the card
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price * quantity)}'),
-          trailing: Text('$quantity x'),
+          direction: DismissDirection.endToStart,
+      child: Card(
+        margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Theme.of(context).primaryColor,
+              child: Padding(
+                  padding: const EdgeInsets.all(5),
+                  child: FittedBox(child: Text('\$$price'))),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('$quantity x'),
+          ),
         ),
       ),
     );
