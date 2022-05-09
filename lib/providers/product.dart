88066028ed2cec7c51af6bf8df8655979e407e76
hhoury <hhoury@hotmail.com>
2022-05-09 17:58:42 +0300

    fav optimistic update

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/http_exception.dart';
 
 class Product with ChangeNotifier {
   final String id;
@@ -16,8 +21,25 @@ class Product with ChangeNotifier {
     required this.imageUrl,
     this.isFavorite = false,
   });
-  void toggleFavoriteStatus() {
+  void toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url = Uri.parse(
+        'https://shop-app-99d19-default-rtdb.firebaseio.com/products/$id.json');
+
+    try {
+      final res =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+      if (res.statusCode >= 400) {
+        isFavorite = oldStatus;
+        notifyListeners();
+        throw HttpException('could not delete product');
+      }
+    } catch (error) {
+      isFavorite = oldStatus;
+      notifyListeners();
+      throw HttpException('could not delete product');
+    }
   }
 }
